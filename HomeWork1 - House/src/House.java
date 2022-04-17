92cea3629e6ebddc6ee8ec8e33a15a181d7ed50f
AlimeShvedova <alime_93@mail.ru>
2022-04-17 16:46:45 +0200

    test commit

---
+++ HomeWork1 - House/src/House.java
@@ -0,0 +1,16 @@
+public class House {
+        public static void main(String[] args) {
+            String house1 = "________________'\"_____";
+            String house2 = "_______________---_____";
+            String house3 = "__________/\\_|_|_______";
+            String house4 = "_________/__\\|_|_______";
+            String house5 = "________/____\\_|_______";
+            String house6 = "_______/______\\|_______";
+            String house7 = "______/___[__]_\\_______";
+            String house8 = "_____/|________|\\______";
+            String house9 = "____/_|________|_\\_____";
+            String house10 = "______|_---____|_______";
+            String house11 = "______|_|_|____|_______";
+            System.out.println(house1 + "\n" + house2 + "\n" + house3 + "\n" + house4 + "\n" + house5 + "\n" + house6 + "\n" + house7 + "\n" + house8 + "\n" + house9 + "\n" + house10 + "\n" + house11);
+        }
+}
